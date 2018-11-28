f781906deec7473879f310524837b17e1b6da59f
Akhil K <akhil.kn@pitsolutions.com>
2018-11-28 16:12:30 +0530

    Set up script & heroku port

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 
@@ -44,6 +44,6 @@ app.get('/bad', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('listening port 3000');
+app.listen(port, () => {
+    console.log('listening port: ' + port);
 })
